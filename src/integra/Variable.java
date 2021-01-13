6c6b1693bde07081edf5056dcdde23f9ae8d94b1
aerodust <ansrltjd992>
2021-01-13 22:44:18 +0900

    생활코딩 공부

---
+++ src/integra/Variable.java
@@ -0,0 +1,11 @@
+package integra;
+
+public class Variable {
+    public static void main(String[] args) {
+        int a = 1; // Number -> integer
+        System.out.println(a);
+
+        double b =1.1;  // real number
+        System.out.println(b);
+    }
+}
