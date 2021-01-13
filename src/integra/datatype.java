5b3ab67677ccd31e61d568b5f9d9848cf417fb9d
aerodust <ansrltjd992>
2021-01-13 22:06:13 +0900

    생활코딩 공부

---
+++ src/integra/datatype.java
@@ -0,0 +1,13 @@
+package integra;
+/** 생활코딩 자바 강의 연습*/
+public class datatype {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(6); // int
+        System.out.println("six"); // String
+        System.out.println("6"); // String 6
+
+        System.out.println(6+6); //12
+        System.out.println("6"+"6"); // 문자열 결합
+    }
+}
--- src/integra/test1.java
+++ src/integra/test1.java
@@ -1,4 +1,7 @@
 package integra;
 
 public class test1 {
-}
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello, M1 Macbook!");
+    }}
