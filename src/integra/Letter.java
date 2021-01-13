112ab8bba7176512fe521b271f2f9f786702e50a
aerodust <ansrltjd992>
2021-01-13 22:52:23 +0900

    생활코딩 공부

---
+++ src/integra/Letter.java
@@ -0,0 +1,14 @@
+package integra;
+
+import java.util.Arrays;
+
+public class Letter {
+    public static void main(String[] args) {
+        String name = "Moon";
+        System.out.println("Hello, "+name+" ... "+name+" ... "+name);
+
+        double VAT = 10.0;
+        System.out.println(VAT);
+
+    }
+}
