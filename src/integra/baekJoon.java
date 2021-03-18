a1666a9d5bb87006d34ff7b60e1124e5c5302492
aerodust <ansrltjd992>
2021-03-18 11:03:58 +0900

    백준문제풀이용

--- src/integra/baekJoon.java
+++ src/integra/baekJoon.java
@@ -1,5 +1,24 @@
 package integra;
-
+import java.util.*;
 public class baekJoon {
+    public static void main(String[] args) {
+        /*System.out.printf("%s", "\\    /\\\n" +
+                                " )  ( ')\n" +
+                                "(  /  )\n" +
+                                " \\(__)|" );
 
+        System.out.printf("%s", "|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+
+                Scanner sc = new Scanner(System.in);
+                String buff = sc.nextLine();
+                String[] array = buff.split(" ");
+                int r = Integer.parseInt(array[0]);
+                int h = Integer.parseInt(array[1]);
+                System.out.println(r+h);*/
+
+    }
 }
